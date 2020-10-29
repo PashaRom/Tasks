0df01a49721b44a1b5dbbc50660ff56d50ccebd6
Павел Ромаш <pashar@tut.by>
2020-10-29 20:56:35 +0300

    first build project: Basic Authorization, Cookies, Modal Windows

---
+++ Task10/Utilities/StringUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace Utilities
+{
+    public static class StringUtil
+    {
+        public static string GeneraterText(int lenght)
+        {
+            var random = new Random();
+            return new String(Enumerable.Range(0, lenght).Select(n => (Char)(random.Next(65, 90))).ToArray());
+        }
+
+        public static int GetIdVkByChar(string elementId, char siporateChar)
+        {
+            string[] seporatedId = elementId.Split(siporateChar);
+            return Convert.ToInt32(seporatedId[seporatedId.Length - 1]);
+        }
+
+        public static int GetAuthorPostIdVk(string attributValue)
+        {
+            string[] seporateId = attributValue.Split("id");
+            return Convert.ToInt32(seporateId[seporateId.Length - 1]);
+        }        
+    }
+}
